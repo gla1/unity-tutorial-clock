e834f9331c6928b5d8253c46003b9175a393fa1b
checkers <21086425+gla1@users.noreply.github.com>
2022-02-05 17:03:29 -0500

    init

---
+++ clocktutorial/Assets/Scripts/Clock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+public class Clock : MonoBehaviour
+{
+
+    [SerializeField]Transform hoursPivot;
+    [SerializeField]Transform minutesPivot;
+    [SerializeField]Transform secondsPivot;
+
+    const float hoursToDegrees = -30;
+    const float minutesToDegrees = -6;
+    const float secondsToDegrees = -6;
+    void Awake (){
+        DateTime time = DateTime.Now;
+        hoursPivot.localRotation = Quaternion.Euler(0,0, hoursToDegrees * time.Hour);
+        minutesPivot.localRotation = Quaternion.Euler(0,0, minutesToDegrees * time.Minute);
+        secondsPivot.localRotation = Quaternion.Euler(0,0, secondsToDegrees * time.Second);
+    }
+    
+    void Update (){
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0,0, hoursToDegrees * (float) time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0,0, minutesToDegrees * (float) time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0,0, secondsToDegrees * (float) time.TotalSeconds);
+    }
+}
